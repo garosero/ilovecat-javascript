97a1a94d6d978d431a139e7d8c2b26699ef041bf
1TBhard <was1383@naver.com>
2020-08-17 15:14:46 +0900

    처음 고양이 과제

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,29 +1,29 @@
-export default class SearchBar {    
+export default class SearchBar {
+	constructor($target, onSearch) {
+		this.$target = $target;
+		this.onSearch = onSearch;
+		this.render();
+	}
 
-    constructor($target, onSearch) {
-        this.$target = $target;
-        this.onSearch = onSearch;
-        this.render();
-    }
+	render() {
+		this.$target.innerHTML = "";
 
-    render() {
-        this.$target.innerHTML = '';
+		const wrapper = document.createElement("div");
+		wrapper.className = "wrapper";
 
-        const wrapper = document.createElement('div');
-        wrapper.className = 'wrapper';
+		const searchBox = document.createElement("input");
+		searchBox.className = "search-box";
+		searchBox.placeholder = "고양이를 검색하세요.";
 
-        const searchBox = document.createElement('input');
-        searchBox.className = 'search-box';
-        searchBox.placeholder = '고양이를 검색하세요.';
+		searchBox.addEventListener("keyup", (e) => {
+			if (e.keyCode == 13) {
+				const keyword = searchBox.value;
+				this.onSearch(keyword);
+			}
+		});
 
-        searchBox.addEventListener('keyup', e => {
-            if(e.keyCode == 13){
-                const keyword = searchBox.value;
-                this.onSearch(keyword);
-            }
-        });
-        
-        wrapper.appendChild(searchBox);
-        this.$target.appendChild(wrapper);
-    }
+		wrapper.textContent = "😺 ";
+		wrapper.appendChild(searchBox);
+		this.$target.appendChild(wrapper);
+	}
 }
